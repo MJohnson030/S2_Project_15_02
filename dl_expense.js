8a55a8b4904bd1a84075839b910570356070c470
Unknown <mjohnson030@west-mec.org>
2019-05-05 21:18:16 -0700

    Final Commit - Ready For Review
    Added comments and finished the Javascript function "calcExp" which calls all of the documents used to calculate the total.

--- dl_expense.js
+++ dl_expense.js
@@ -31,6 +31,7 @@
       
 */
 
+//Loads the load event using an event listener which also has an anonymous function attached
 window.addEventListener("load", function () {
       var changingCells = document.getElementById("travelExp").oninput;
 
@@ -42,16 +43,46 @@ window.addEventListener("load", function () {
 
 });
 
+//checks for valiidity of the summary and displays a custom message if the value of the summary is missing
+function validateSummary() {
+      var summary = document.getElementById("summary");
+      if (summary.validity.valueMissing) {
+            summary.setCustomValidity("You must include a summary of the trip in your report.")
+      } else {
+            summary.setCustomValidity("");
+      }
+}
+
+
+
 function calcClass(sumClass) {
+      //sets the value of the sumFields variable to the object collection of all elements with the class name of "sumClass".
       var sumFields = document.getElementsByClassName("sumClass");
       var sumTotal = 0;
 
       for (var i = 0; i < sumFields.length; i++) {
             var itemValue = parseFloat(sumFields[i].value);
-            if (!isNaN(itemValue)) {}
+            if (!isNaN(itemValue)) {
+                  sumTotal += itemValue;
+            }
       }
 }
 
+
+function calcExp() {
+      var expTable = document.querySelectorAll("table#travelExp tbody tr");
+      
+      for (var i = 0; i < expTable.length; i++) {
+            document.getElementById("subtotal" + i).value = formatNumber(calcClass("date" + i), 2);
+      }
+      
+      document.getElementById("transTotal").value = formatNumber(calcClass("trans"), 2);
+      document.getElementById("lodgeTotal").value = formatNumber(calcClass("lodge"), 2);
+      document.getElementById("mealTotal").value = formatNumber(calcClass("meal"), 2);
+      document.getElementById("otherTotal").value = formatNumber(calcClass("other"), 2);
+      // Sets the value of the expTotal input element to the value returned by the calcClass() function. it is being formatted using the formatUSCurrency() function.
+      document.getElementById("expTotal").value = formatUSCurrency(calcClass("sum"));
+}
 
 function formatNumber(val, decimals) {
       return val.toLocaleString(undefined, {
